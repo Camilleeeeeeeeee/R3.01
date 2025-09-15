595bcef9cd9113588c2fdb87eff4312d371f1d71
Camilleeeeeeeeee <camille.golliot@etu.unicaen.fr>
2025-09-15 15:03:04 +0200

    Commit après l'exo 1-2-3 de la ressource

--- script/script.js
+++ script/script.js
@@ -1,2 +1,20 @@
-  git config --global user.email "camille.golliot@etu.unicaen.fr"
-  git config --global user.name "Camilleeeeeeeeee"
+//Exercice 1
+
+let degreC = 22;
+let degreF = degreC * 9/5 + 32;
+
+console.log("Température en degrés : " + degreC + ", et température en Fahrenheit : " + degreF);
+
+//Exercice 2
+
+let longueur = 5;
+let largeur = 4;
+
+console.log("L'aire du rectangle (longueur x largeur) : " + largeur*longueur);
+
+//Exercice 3
+
+let prenom = "Camille";
+let nom = "Golliot";
+
+console.log("Je m'appelle est : " + prenom + " " + nom);
