dd1fd63d8b4e3a94d9cf9e3fa4307d4f9938e4d8
Camilleeeeeeeeee <camille.golliot@etu.unicaen.fr>
2025-09-16 17:21:34 +0200

    Shifumi qui fonctionne et affiche qui a gagné

--- script/shifumi.js
+++ script/shifumi.js
@@ -1,9 +1,30 @@
 let button = document.querySelectorAll('.choice');
+const result = document.getElementById('result');
+const choices = ["Pierre", "Feuille", "Ciseaux"];
 
 button.forEach(button => {
     button.addEventListener( 'click', () => {
         const playerChoice = button.textContent;
-        console.log(playerChoice);
+        const robotChoice = choices[Math.floor(Math.random() * 3)];
 
+        let resultNow;
+
+        console.log(playerChoice);
+        console.log(robotChoice);
+
+        if(playerChoice == robotChoice){
+            resultNow = "draw"
+        }
+
+        if(playerChoice == "Pierre" && robotChoice == "Ciseaux" ||
+            playerChoice == "Feuille" && robotChoice == "Pierre" ||
+            playerChoice == "Ciseaux" && robotChoice == "Feuille"){
+                resultNow = "win"
+        }
+        else{
+            resultNow = "loose"
+        }
+
+        result.innerHTML = `I play : ${playerChoice}<br>The robot plays : ${robotChoice}<br>It's a <strong>${resultNow === 'draw' ? 'Draw !!' : resultNow === 'win' ? 'Win !!' : 'Looser AHAH !!'}</strong>`;
     });
 });
