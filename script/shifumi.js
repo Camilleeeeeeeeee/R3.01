65080e70c69199f78a33c0b34ba841d4d0be7f49
Camilleeeeeeeeee <camille.golliot@etu.unicaen.fr>
2025-09-16 17:55:17 +0200

    L'affichage des scores ne se met pas à jour

--- script/shifumi.js
+++ script/shifumi.js
@@ -1,6 +1,15 @@
 let button = document.querySelectorAll('.choice');
 const result = document.getElementById('result');
 const choices = ["Pierre", "Feuille", "Ciseaux"];
+let win = 0;
+let loose = 0;
+let draw = 0;
+
+function majResult(){
+    labelwin.textContent = "Number of wins : " + win;
+    labelloose.textContent = "Number of looses : " + loose;
+    labeldraw.textContent = "Number of draws : " + draw;
+}
 
 button.forEach(button => {
     button.addEventListener( 'click', () => {
@@ -14,17 +23,24 @@ button.forEach(button => {
 
         if(playerChoice == robotChoice){
             resultNow = "draw"
+            draw++;
+            majResult();
         }
 
         if(playerChoice == "Pierre" && robotChoice == "Ciseaux" ||
             playerChoice == "Feuille" && robotChoice == "Pierre" ||
             playerChoice == "Ciseaux" && robotChoice == "Feuille"){
                 resultNow = "win"
+                win++;
+                majResult();
         }
         else{
             resultNow = "loose"
+            loose++;
+            majResult();
         }
 
         result.innerHTML = `I play : ${playerChoice}<br>The robot plays : ${robotChoice}<br>It's a <strong>${resultNow === 'draw' ? 'Draw !!' : resultNow === 'win' ? 'Win !!' : 'Looser AHAH !!'}</strong>`;
+    
     });
 });
